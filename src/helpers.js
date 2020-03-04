7fc0069d21bc067a98cda581210eee842ec7f3f5
viricruz <viridiana.cruz.olivera@gmail.com>
2020-03-04 15:00:38 -0700

    Fix eslint errors

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,3 @@
-const extractNumbers = (str) => str.match(/-?\d+(?:\.\d+)?/g).map(Number)[0];
+const extractNumbers = str => str.match(/-?\d+(?:\.\d+)?/g).map(Number)[0];
 
 export default extractNumbers;
